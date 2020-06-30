a42029b177872fff4e8876fd24f2fc300176d06a
ct0622 <2206107682@qq.com>
2020-06-30 18:02:25 +0800

    6.30

---
+++ blade-service-api/fee-api/src/main/java/org/springblade/fee/vo/Item.java
@@ -0,0 +1,15 @@
+package org.springblade.fee.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+public class Item implements Serializable {
+
+
+	private static final long serialVersionUID = -8242386673141999492L;
+
+	private  String item_type_name;
+}
--- blade-service/fee/src/main/java/org/springblade/fee/service/impl/FeeServiceImpl.java
+++ blade-service/fee/src/main/java/org/springblade/fee/service/impl/FeeServiceImpl.java
@@ -37,41 +37,46 @@ public class FeeServiceImpl extends BaseServiceImpl<FeeMapper, Applicationfrom> 
 	 */
 	@Override
 	public List<Fee> queryapplicationfrom(List<Long> request_id_list) {
-
+		//申请单列表
 		List<Fee> fees = baseMapper.selectFeeList(request_id_list);
+		//申请单详情
 		for(Fee fee:fees){
 			int requestType = fee.getRequestType();
 			DicRequestType dicRequestType =new DicRequestType();
 			dicRequestType.setId(requestType);
 			DicRequestType dicrequesttype = baseMapper.selectDicRequestTypeById(dicRequestType);
+			//申请单类别名称
 			String text = dicrequesttype.getText();
+			//科室id
 			int deptId = fee.getDeptId();
+			//医生id
 			int doctorId = fee.getDoctorId();
+			//开单科室
 			String urldeptId = "";
 			Map<String, Object> map = new HashMap<String, Object>();
 			map.put("dept_id", deptId);
 			String res = CommonUtil.doGet(urldeptId, map);
 			Dept dept = JSONObject.parseObject(res, Dept.class);
+			//开单医生
 			String urldoctorId = "";
 			Map<String, Object> mapdoctor = new HashMap<String, Object>();
 			mapdoctor.put("staff_id", doctorId);
 			String resdoctor = CommonUtil.doGet(urldoctorId, mapdoctor);
 			StaffInfo staffinfo = JSONObject.parseObject(resdoctor, StaffInfo.class);
+			//收费项目
 			int itemId = fee.getItemId();
 			String urlitemId = "";
 			Map<String, Object> mapitem = new HashMap<String, Object>();
 			mapitem.put("item_id", itemId);
 			String resitem = CommonUtil.doGet(urlitemId, mapitem);
 			ItemChargeBrief itemchargebrief = JSONObject.parseObject(resitem, ItemChargeBrief.class);
+			//收费项目类型名称
 			int item_type_id = itemchargebrief.getItem_type_id();
-
 			String urlitemtypeid= "";
 			Map<String, Object> mapitemtype = new HashMap<String, Object>();
 			mapitemtype.put("item_type_id", item_type_id);
 			String resitemtype = CommonUtil.doGet(urlitemtypeid, mapitemtype);
-//			ItemChargeBrief itemchargebrief = JSONObject.parseObject(resitemtype, ItemChargeBrief.class);
-
-
+			Item item = JSONObject.parseObject(resitemtype, Item.class);
 
 		}
 
