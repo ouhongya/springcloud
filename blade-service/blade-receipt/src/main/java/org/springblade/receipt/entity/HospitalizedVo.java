5476f752d0ac13f94961f9ab51a46212c211a57a
OuHong <ouhongcc@163.com>
2020-07-03 17:49:13 +0800

    费用明细模块

--- blade-service/blade-receipt/src/main/java/org/springblade/receipt/entity/HospitalizedVo.java
+++ blade-service/blade-receipt/src/main/java/org/springblade/receipt/entity/HospitalizedVo.java
@@ -17,4 +17,52 @@ public class HospitalizedVo {
 	private BigDecimal payMoney;
 	@ApiModelProperty(value = "剩余费用")
 	private BigDecimal remainingMoney;
+
+	public String getRequestId() {
+		return requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getFeePaid() {
+		return feePaid;
+	}
+
+	public void setFeePaid(String feePaid) {
+		this.feePaid = feePaid;
+	}
+
+	public String getPaidTime() {
+		return paidTime;
+	}
+
+	public void setPaidTime(String paidTime) {
+		this.paidTime = paidTime;
+	}
+
+	public BigDecimal getTotalMoney() {
+		return totalMoney;
+	}
+
+	public void setTotalMoney(BigDecimal totalMoney) {
+		this.totalMoney = totalMoney;
+	}
+
+	public BigDecimal getPayMoney() {
+		return payMoney;
+	}
+
+	public void setPayMoney(BigDecimal payMoney) {
+		this.payMoney = payMoney;
+	}
+
+	public BigDecimal getRemainingMoney() {
+		return remainingMoney;
+	}
+
+	public void setRemainingMoney(BigDecimal remainingMoney) {
+		this.remainingMoney = remainingMoney;
+	}
 }
