f65aa1bfddf591a68d08c31845fbce5fe6570cd1
yaodan <3034008462@qq.com>
2020-07-04 10:10:33 +0800

    费用类别

---
+++ blade-service-api/blade-sysettings-api/src/main/java/org/springblade/dictionary/Category.java
@@ -0,0 +1,31 @@
+package org.springblade.dictionary;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springblade.core.mp.base.BaseEntity;
+
+import java.io.Serializable;
+
+/**
+ * [费用类别字典表--category]
+ *
+ * @Author create by
+ * @date 2020/7/4-9:46
+ */
+@Data
+@TableName("category")
+public class Category extends BaseEntity implements Serializable {
+
+	/**
+	 * 费用类别id
+	 */
+	@ApiModelProperty(value = "费用类别id")
+	private Long categoryId;
+	/**
+	 * 费用类别名称
+	 */
+	@ApiModelProperty(value = "费用类别名称")
+	private String text;
+}
+
---
+++ blade-service/blade-sysettings/src/main/java/org/springblade/sysettings/controller/CategoryController.java
@@ -0,0 +1,55 @@
+package org.springblade.sysettings.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.AllArgsConstructor;
+import org.springblade.core.tool.api.R;
+import org.springblade.dictionary.Category;
+import org.springblade.sysettings.service.ICategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/api/category")
+@RestController
+@AllArgsConstructor
+public class CategoryController {
+	private ICategoryService iCategoryService;
+
+	/**
+	 * [查询所有]
+	 *
+	 * @return
+	 */
+	@GetMapping("/selectAll")
+	@ApiOperation(value = "费用类别字典表", notes = "费用类别字典表")
+	public R selectAll() {
+		return R.data(iCategoryService.selectAll());
+	}
+
+	/**
+	 * [根据id查询]
+	 *
+	 * @param categoryId
+	 * @return
+	 */
+	@GetMapping("/selectById")
+	@ApiOperation(value = "费用类别字典表", notes = "费用类别字典表")
+	public R selectById(@ApiParam(value = "费用类别id") Long categoryId) {
+		return R.data(iCategoryService.selectById(categoryId));
+	}
+
+	/**
+	 * [新增数据]
+	 *
+	 * @param
+	 * @return
+	 */
+	@GetMapping("/save")
+	@ApiOperation(value = "费用类别字典表", notes = "费用类别字典表")
+	public R save(@ApiParam(value = "费用类别") Category category) {
+		return R.data(iCategoryService.insert(category));
+	}
+
+}
