165fce8f739aa716ab95db24a9e035652cec8ee3
OuHong <ouhongcc@163.com>
2020-07-07 18:00:56 +0800

    费用明细模块

--- blade-service/blade-receipt/src/main/java/org/springblade/receipt/entity/PayAaliCallBack.java
+++ blade-service/blade-receipt/src/main/java/org/springblade/receipt/entity/PayAaliCallBack.java
@@ -2,8 +2,7 @@ package org.springblade.receipt.entity;
 
 import java.math.BigDecimal;
 
-public class PayAaliCallBack {
-
+public  class  PayAaliCallBack {
 	private String sign;
 
 	public String getSign() {
@@ -14,64 +13,7 @@ public class PayAaliCallBack {
 		this.sign = sign;
 	}
 
-	class alipay_trade_page_refund_response {
-		private String code;
-		private String msg;
-		private String trade_no;
-		private String out_trade_no;
-		private String out_request_no;
-		private BigDecimal refund_amount;
-
-		public String getCode() {
-			return code;
-		}
-
-		public void setCode(String code) {
-			this.code = code;
-		}
-
-		public String getMsg() {
-			return msg;
-		}
-
-		public void setMsg(String msg) {
-			this.msg = msg;
-		}
-
-		public String getTrade_no() {
-			return trade_no;
-		}
-
-		public void setTrade_no(String trade_no) {
-			this.trade_no = trade_no;
-		}
-
-		public String getOut_trade_no() {
-			return out_trade_no;
-		}
-
-		public void setOut_trade_no(String out_trade_no) {
-			this.out_trade_no = out_trade_no;
-		}
-
-		public String getOut_request_no() {
-			return out_request_no;
-		}
-
-		public void setOut_request_no(String out_request_no) {
-			this.out_request_no = out_request_no;
-		}
-
-		public BigDecimal getRefund_amount() {
-			return refund_amount;
-		}
-
-		public void setRefund_amount(BigDecimal refund_amount) {
-			this.refund_amount = refund_amount;
-		}
-	}
-
-	alipay_trade_page_refund_response alipay_trade_page_refund_response;
+	private alipay_trade_page_refund_response alipay_trade_page_refund_response;
 
 	public org.springblade.receipt.entity.alipay_trade_page_refund_response getAlipay_trade_page_refund_response() {
 		return alipay_trade_page_refund_response;
@@ -81,4 +23,5 @@ public class PayAaliCallBack {
 		this.alipay_trade_page_refund_response = alipay_trade_page_refund_response;
 	}
 }
+
 
